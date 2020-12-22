f86273be45a1b612c5569817d555db93325a852c
hiroshimashu <hiroshimashu@gmail.com>
2020-12-22 20:31:25 +0900

    fix: linked list to biddirectional-linked-list

--- C++ practice/LinkedList/linked_list.cpp
+++ C++ practice/LinkedList/linked_list.cpp
@@ -13,15 +13,16 @@
 using namespace std;
 
 struct Node {
-    Node* next;
+    Node *next, *prev;
     string name;
-    Node(string name_ = "") : next(NULL), name(name_) {}
+    Node(string name_ = "") : next{NULL}, name{name_}, prev{NULL} { }
 };
 
 Node* nil;
 
 void init() {
     nil = new Node();
+    nil -> prev = nil ;
     nil -> next = nil;
 }
 
@@ -30,11 +31,21 @@ void printList() {
     for(; cur != nil; cur = cur -> next) {
         cout << cur->name << " -> ";
     }
+    cout << endl;
 }
 
 void insert(Node* v, Node* p = nil) {
-    v -> next = p -> next;
-    p -> next = v;
+    v ->next = p -> next;
+    p ->next->prev = v;
+    p->next = v;
+    v->prev = p;
+}
+
+void erase(Node *v) {
+    if (v == nil) return;
+    v->prev->next = v->next;
+    v->next->prev = v->prev;
+    delete v;
 }
 
 int main() {
@@ -52,6 +63,6 @@ int main() {
         insert(node);
         
         cout << "step " << i << ": ";
-        printList();`
+        printList();
     }
 }
